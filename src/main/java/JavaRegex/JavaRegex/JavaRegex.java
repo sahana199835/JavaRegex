55f700a1b80b276d67258174fa47cdbfe4e84ac1
sahana199835 <tnsahana1@gmail.com>
2023-04-15 20:08:27 +0530

    add:check valid email

--- src/main/java/JavaRegex/JavaRegex/JavaRegex.java
+++ src/main/java/JavaRegex/JavaRegex/JavaRegex.java
@@ -9,37 +9,29 @@ public class JavaRegex {
 	
 	public static void main(String[] args) {
 		String regex = "^[A-Z][a-zA-Z]{2,}$";
-		String regex = "^[A-Z][a-zA-Z]{2,}$";
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The First Name : ");
 		String firstName = sc.nextLine();
 		System.out.println("Enter The Last Name : ");
 		String lastName = sc.nextLine();
-
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter The First Name : ");
+		System.out.println("Enter Email Address: ");
 		String data = sc.nextLine();
-
 
 		Pattern pattern = Pattern.compile(regex);
 
-		Matcher matcher = pattern.matcher(data);
 		Matcher matcher = pattern.matcher(firstName + lastName);
+		Matcher matcher = pattern.matcher(data);
 		boolean result = matcher.matches();
 
-	    if (result) {
-	        System.out.println("Valid first name");
-	    } else {
-	        System.out.println("Pattern not matched");
-	    }
 		if (result) {
 			System.out.println("Valid first name and last name");
+			System.out.println("Valid Email");
 		} else {
 			System.out.println("Pattern not matched");
+			System.out.println("Invalid Email");
 		}
 
-}
 	}
-
-
+}
